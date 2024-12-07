ae2c7bcc8a2313755e324ee5a11ccdf0719bd598
prasanna <prasannasunuwar03@gmail.com>
2024-12-07 09:57:00 +0545

    validation sign in page

---
+++ script.js
@@ -0,0 +1,42 @@
+// --------------------------------for cart------------------------------
+function updateQuantity(button, change) {
+    const quantityElement = button.parentNode.querySelector('.cart-quantity');
+    let quantity = parseInt(quantityElement.textContent);
+    quantity += change;
+
+    if (quantity < 1) {
+        quantity = 1;
+    }
+
+    quantityElement.textContent = quantity;
+    updateTotal(button.closest('.cart-item'));
+}
+
+function updateTotal(item) {
+    const price = parseFloat(item.querySelector('td:nth-child(2)').textContent.replace('Rs.', '').trim());
+    const quantity = parseInt(item.querySelector('.cart-quantity').textContent);
+    const total = price * quantity;
+    item.querySelector('.cart-total').textContent = `Rs.${total.toFixed(2)}`;
+    updateCartTotal();
+}
+
+function updateCartTotal() {
+    const allItems = document.querySelectorAll('.cart-item');
+    let cartTotal = 0;
+
+    allItems.forEach(item => {
+        const itemTotal = parseFloat(item.querySelector('.cart-total').textContent.replace('Rs.', '').trim());
+        cartTotal += itemTotal;
+    });
+
+    document.getElementById('cart-total-price').textContent = `Rs.${cartTotal.toFixed(2)}`;
+}
+
+function removeItem(button) {
+    const item = button.closest('.cart-item');
+    item.remove();
+    updateCartTotal();
+}
+
+
+
